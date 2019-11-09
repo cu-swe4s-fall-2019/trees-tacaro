77139ccc6959064fc3cb2d69f1f46c4e8a457294
tacaro <tristan.caro@colorado.edu>
2019-11-08 18:29:13 -0700

    fixed recursion depth error

--- binary_tree.py
+++ binary_tree.py
@@ -5,42 +5,58 @@ class Node:
     left: an object of class Node. Represents left child of Node.
     right: an object of class Node. Represents right child of Node.
     '''
-    def __init__(self, key=None, value=None, left=None, right=None):
+    def __init__(self, key, value=None, left=None, right=None):
         self.key = key
         self.value = value
         self.left = left
         self.right = right
 
 
-def insert(root, key, value):
-    '''
-    root: an object of class Node: class
-    key: an item representing the storage key: int, str
-    value: the item represented by the key: str, int, float, list, etc.
-    '''
-    if root.key is None:
-        root.key = key
-        root.value = value
-    elif key > root.key:
-        insert(root.right, key, value)
-    elif key < root.key:
-        insert(root.left, key, value)
-    else:
-        return -1
+class BinaryTree:
+    def __init__(self, keys=None, values=None):
+        self.root = None
 
+    def insert(self, k, value=None):
+        '''
+        key: an item representing the storage key: int, str
+        value: the item represented by the key: str, int, float, list, etc.
+        '''
+        if self.root is None:
+            self.root = Node(k, value)
+        else:
+            self.branch(k, value, self.root)
 
-def search(root, key):
-    '''
-    root: an object of class Node: class
-    key: a key whose corresponding value you wish to return: int, str
-    '''
-    if root.key is None:
-        raise ValueError("Root key is None")
-    if root.key == key:
-        return root.value
-    elif key < root.key:
-        search(root.left, key)
-    elif key > root.key:
-        search(root.right, key)
-    else:
-        return -1
+    def branch(self, key, value, node):
+        """Branching of tree"""
+        if key < node.key:
+            if node.left is not None:
+                self.branch(key, value, node.left)
+            else:
+                node.left = Node(key, value)
+        else:
+            if node.right is not None:
+                self.branch(key, value, node.right)
+            else:
+                node.right = Node(key, value)
+
+    def search(self, key):
+        '''
+        self: an object of class Node: class
+        key: a key whose corresponding value you wish to return: int, str
+        '''
+        if self.root is None:
+            return -1
+        else:
+            return self.locate(key, self.root)
+
+    def locate(self, k, node):
+        """Locates correct branch --> correct node, based on k"""
+        if k == node.key:
+            return node
+        elif node.right is None and node.left is None:
+            return None
+            print("k not found!")
+        elif k < node.key and node.left is not None:
+            return self.locate(k, node.left)
+        elif k > node.key and node.right is not None:
+            return self.locate(k, node.right)
